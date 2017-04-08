00b3a9ea456475563350309cabe7e8034360275c
Arda Mavi <ardamavi2@gmail.com>
2017-04-08 14:18:19 +0300

    Train and test processing and neural network model updated

--- main.py
+++ main.py
@@ -4,22 +4,24 @@ from os import listdir
 from img_procedure import *
 from neural_network_procedure import *
 
-def getTrainData():
+def getDatasetsFromDir(datasets_dir):
+    # This function return training or tasting input(X: Image matris) and output(y: Decimal character) datasets from all directory on datasets_dir
     # X is training data and y is label:
-    X, y = [], []
-
-    # We get the training datasets from training images file:
-    train_dir = 'Data/images/train'
+    X_train, y_train, X_test, y_test = [], [], [], []
 
     # Geting training characters:
     try:
-        char_dirs = listdir(train_dir)
+        char_dirs = listdir(datasets_dir)
     except:
         return None, None
 
+    # '.DS_Store' is not a character directory, delete it:
+    if '.DS_Store' in char_dirs:
+        char_dirs.remove('.DS_Store')
+
     # Looking for a character images is not null:
     for char_dir in char_dirs:
-        if len(listdir(train_dir+'/'+char_dir)) < 1:
+        if len(listdir(datasets_dir+'/'+char_dir)) < 1:
             char_dirs.remove(char_dir)
         else:
             continue
@@ -28,10 +30,25 @@ def getTrainData():
         return None, None
     # Getting image datasets from all character dir:
     for char_dir in char_dirs:
-        for img_dir in listdir(train_dir+'/'+char_dir):
-            X.append(getImg(train_dir+'/'+char_dir+'/'+img_dir))
-            y.append(ord(char_dir))
-    return X, y
+        img_dirs = listdir(datasets_dir+'/'+char_dir)
+
+        # '.DS_Store' is not a image directory, delete it:
+        if '.DS_Store' in img_dirs:
+            img_dirs.remove('.DS_Store')
+
+        # Split the traning and test images:
+        point = int(0.9*len(img_dirs))
+        train_imgs, test_imgs = img_dirs[:point], img_dirs[point:]
+
+        # Createing train and test image matrix list:
+        for img_dir in train_imgs:
+            X_train.append(getImg(datasets_dir+'/'+char_dir+'/'+img_dir))
+            y_train.append(ord(char_dir))
+        for img_dir in test_imgs:
+            X_test.append(getImg(datasets_dir+'/'+char_dir+'/'+img_dir))
+            y_test.append(ord(char_dir))
+
+    return X_train, y_train, X_test, y_test
 
 def main():
     # Get image directory:
@@ -53,16 +70,24 @@ def main():
     # If you have not saved Neural Network already, create new:
     if clf is None:
         clf = createNeuralNetwork()
-        X, y = getTrainData()
-        if X is None:
-            print('Training datasets not found!')
+        # We get the training datasets from training images file:
+        X_train, y_train, X_test, y_test = getDatasetsFromDir('Data/images/train')
+        if X_train is None:
+            print('Characters datasets not found!')
             return
-        # TODO: Make regular X data
-        clf = trainNeuralNetwork(clf, X, y)
+
+        # Training neural network
+        clf = trainNeuralNetwork(clf, X_train, y_train)
+
+        # Saveing neural network:
         saveNeuralNetwork(clf)
 
+        # Scores:
+        print('Train Score:', getScore(clf, X_train, y_train))
+        print('Test Score:', getScore(clf, X_test, y_test))
+
     # Predict:
-    print('Predict:', chr(NeuralNetworkPredict(clf, img)))
+    print('Predict:', chr(getPredict(clf, img)))
 
     # Finish main:
     return
--- neural_network_procedure.py
+++ neural_network_procedure.py
@@ -4,11 +4,7 @@ from sklearn.neural_network import MLPClassifier
 
 def createNeuralNetwork():
     # Createing Neural Network:
-    return MLPClassifier(activation='tanh', hidden_layer_sizes=(18, 5))
-
-def saveNeuralNetwork(clf):
-    # Save classifier:
-    joblib.dump(clf, 'Data/neural_network/classifier.pkl')
+    return MLPClassifier(hidden_layer_sizes=(40, 28, 10))
 
 def getNeuralNetwork():
     # Getting trained Neural Network:
@@ -22,6 +18,13 @@ def trainNeuralNetwork(clf, X, y):
     # Training Neural Network:
     return clf.fit(X, y)
 
-def NeuralNetworkPredict(clf, img):
+def getScore(clf, X, y):
+    return clf.score(X, y)
+
+def getPredict(clf, img):
     # Get predict:
     return clf.predict(img)
+
+def saveNeuralNetwork(clf):
+    # Save classifier:
+    joblib.dump(clf, 'Data/neural_network/classifier.pkl')
